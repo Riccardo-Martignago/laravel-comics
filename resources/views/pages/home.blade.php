4da99615b714a0219a378653a128199e43db51c3
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-07-16 18:56:34 +0200

    Aggiunta opzione per passare nella sezione shop con articoli al suo interno

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,34 +1,4 @@
 @extends('layouts.app')
 
 @section('main-content')
-    <section class="bg-black">
-        <article class="container d-flex flex-wrap">
-            @foreach($data as $dato)
-                <div class="card" style="width: 18rem;">
-                    <img src="{{ $dato['thumb'] }}" alt="{{ $dato['title'] }}">
-                    <div class="card-body">
-                        <h2>
-                            {{ $dato['title'] }}
-                        </h2>
-                    </div>
-                </div>
-            @endforeach
-        </article>
-    </section>
-    <section class="bg-blue">
-        <div class="container around">
-            <article>
-                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="digital-comics">
-                <p>DIGITAL COMICS</p>
-                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="merchandise">
-                <p>DC MERCHANDISE</p>
-                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="subscriptions">
-                <p>SUBSCRIPTION</p>
-                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="shop">
-                <p>COMIC SHOP LOCATOR</p>
-                <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="power">
-                <p>DC POWER VISA</p>
-            </article>
-        </div>
-    </section>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,16 +4,43 @@
             <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
             <nav>
                 <ul>
-                    <li>characters</li>
-                    <li>comics</li>
-                    <li>movies</li>
-                    <li>tv</li>
-                    <li>games</li>
-                    <li>collectibles</li>
-                    <li>videos</li>
-                    <li>fans</li>
-                    <li>news</li>
-                    <li>shop</li>
+                    <li>
+                        <a href="">CHARACTERS</a> 
+                    </li>
+                    <li>
+                        <a href="">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="">TV</a>
+                    </li>
+                    <li>
+                        <a href="">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="">COLLECTABLES</a>
+                    </li>
+                    <li>
+                        <a href="">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="">FANS</a>
+                    </li>
+                    <li>
+                        <a href="">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="/products">SHOP</a>
+                    </li>
+                    <li>
+                        <form action="" class="d-flex">
+                            <input type="text">
+                            <button type="submit"></button>
+                        </form>
+                        
+                    </li>
                 </ul>
             </nav>
         </section>
